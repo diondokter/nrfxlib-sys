fc65b3766035b1dc7d812a7ec363c9ba127414a4
Dion Dokter <dion@tweedegolf.com>
2024-06-04 13:58:53 +0200

    Enough mock to get to sending

--- mock.c
+++ mock.c
@@ -5,6 +5,11 @@
 #include "nrf_modem_gnss.h"
 #include "nrf_modem.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+static nrf_modem_at_notif_handler_t at_notif_callback = NULL;
+extern void nrf_modem_os_log_rust(int level, const char *formatted_string);
 
 int nrf_fcntl(int fd, int cmd, int flags)
 {
@@ -29,6 +34,8 @@ int nrf_connect(int socket, const struct nrf_sockaddr *address, nrf_socklen_t ad
 
 ssize_t nrf_send(int socket, const void *buffer, size_t length, int flags)
 {
+    nrf_modem_os_log(1, "Send! %i, 0x%X, %i, %i", socket, buffer, length, flags);
+
     if (buffer == NULL || length == 0)
     {
         nrf_modem_os_errno_set(NRF_EINVAL);
@@ -49,24 +56,78 @@ int nrf_close(int fildes)
     return 0;
 }
 
+
+const char *process_at(const char *fmt, va_list args)
+{
+    char buffer[256] = {0};
+
+    vsnprintf(buffer, 256, fmt, args);
+
+    nrf_modem_os_log_rust(2, buffer);
+
+    if (!strcmp(buffer, "AT+CFUN?"))
+    {
+        return "+CFUN: 0\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT+CGSN"))
+    {
+        return "352656100367872\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT+CGMR"))
+    {
+        return "mfw_nrf9160_1.1.1\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT%XICCID"))
+    {
+        return "%XICCID: 8901234567012345678F\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT+CEREG?"))
+    {
+        return "+CEREG: 2,5\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT%XTEMP?"))
+    {
+        return "%XTEMP: 50\r\nOK\r\n";
+    }
+    if (!strcmp(buffer, "AT%XMONITOR"))
+    {
+        return "%XMONITOR: 1,\"EDAV\",\"EDAV\",\"26295\",\"00B7\",7,4,\"00011B07\",7,2300,63,39,\"\", \"11100000\",\"11100000\",\"11100000\"\r\nOK\r\n";
+    }
+
+    if (!strcmp(buffer, "AT%NCELLMEAS=4,6\r\n"))
+    {
+        at_notif_callback("%NCELLMEAS: 0,\"002B670D\",\"20408\",\"8729\",62,11447,6400,424,52,23,10002,1,1,6400,408,47,13,0,\"002B670D\",\"20408\",\"8729\",62,11447,6400,424,52,23,10002,1,1,6400,408,47,13,0");
+    }
+
+    return "OK\r\n";
+}
+
 int nrf_modem_at_cmd(void *buf, size_t len, const char *fmt, ...)
 {
-    strncpy(buf, "OK", len);
+    va_list args;
+    va_start(args, fmt);
+    strncpy(buf, process_at(fmt, args), len);
+    va_end(args);
     return 0;
 }
 
 int nrf_modem_at_printf(const char *fmt, ...)
 {
+    va_list args;
+    va_start(args, fmt);
+    process_at(fmt, args);
+    va_end(args);
     return 0;
 }
 
 int nrf_modem_at_cmd_async(nrf_modem_at_resp_handler_t callback, const char *fmt, ...)
 {
-    callback("OK");
+    va_list args;
+    va_start(args, fmt);
+    callback(process_at(fmt, args));
+    va_end(args);
     return 0;
 }
-
-static nrf_modem_at_notif_handler_t at_notif_callback = NULL;
 
 int nrf_modem_at_notif_handler_set(nrf_modem_at_notif_handler_t callback)
 {
@@ -144,6 +205,12 @@ void nrf_freeaddrinfo(struct nrf_addrinfo *ai)
 {
 }
 
-int nrf_modem_init(const struct nrf_modem_init_params *init_params) {
+int nrf_modem_init(const struct nrf_modem_init_params *init_params)
+{
     return 0;
+}
+
+bool nrf_modem_is_initialized(void)
+{
+    return true;
 }
